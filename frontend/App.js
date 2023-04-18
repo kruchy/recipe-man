9bf766a0a0b36f9bc6272b0925806a0555366693
krzysiek <kruchy61@gmail.com>
2023-04-18 12:58:09 -0700

    Add error handling and autocomplete

--- frontend/App.js
+++ frontend/App.js
@@ -1,50 +1,98 @@
-import React, { useState } from "react";
-import Select from "react-select";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
 
 const knownIngredients = [
-  { value: "cebula", label: "Cebula" },
-  // Dodaj więcej składników, jeśli chcesz
+  "jajko",
+  "mąka",
+  "cukier",
+  "sól",
+  "pieprz",
+  // Dodaj więcej znanych składników tutaj...
 ];
 
 function App() {
-  const [selectedIngredients, setSelectedIngredients] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
   const [recipes, setRecipes] = useState([]);
+  const [error, setError] = useState(null);
 
-  const handleSearch = async () => {
-    const response = await fetch("http://localhost:3001/generateRecipes", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ ingredients: selectedIngredients.map((ingredient) => ingredient.value) }),
-    });
+  useEffect(() => {
+    if (error) {
+      const timer = setTimeout(() => setError(null), 10000);
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
 
-    const generatedRecipes = await response.json();
-    setRecipes(generatedRecipes);
+  const handleSearchRecipes = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/generateRecipes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ingredients }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to generate recipes");
+      }
+
+      const data = await response.json();
+      setRecipes(data);
+    } catch (error) {
+      console.error(error);
+      setError("Wystąpił chwilowy błąd. Spróbuj ponownie później.");
+    }
   };
 
   return (
     <div className="App">
+      {error && (
+        <div className="error-notification" style={notificationStyle}>
+          {error}
+        </div>
+      )}
       <h1>RecipeMan</h1>
-      <Select
-        options={knownIngredients}
-        isMulti
-        onChange={(selected) => setSelectedIngredients(selected)}
-        value={selectedIngredients}
-        className="select-ingredients"
-      />
-      <button onClick={handleSearch}>Szukaj przepisów</button>
-      <div className="recipes">
+      <div className="search-container">
+        <Autocomplete
+          multiple
+          options={knownIngredients}
+          onChange={(event, value) => setIngredients(value)}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="outlined"
+              label="Wprowadź składniki"
+            />
+          )}
+        />
+        <button onClick={handleSearchRecipes}>Szukaj przepisów</button>
+      </div>
+      <div className="recipes-container">
         {recipes.map((recipe, index) => (
           <div key={index} className="recipe">
             <img src={recipe.image} alt={recipe.title} />
-            <div className="recipe-text">
+            <div>
               <h2>{recipe.title}</h2>
               <p>{recipe.description}</p>
             </div>
           </div>
-      ))}
+        ))}
       </div>
     </div>
-    
-    );
-    }
+  );
+}
+
+const notificationStyle = {
+  position: "fixed",
+  top: "20px",
+  right: "20px",
+  backgroundColor: "rgba(255, 0, 0, 0.8)",
+  padding: "10px",
+  borderRadius: "5px",
+  zIndex: 1000,
+  color: "white",
+};
+
+export default App;
