0956321a6341941d3f21bec2650df714468cf53a
krzysiek <kruchy61@gmail.com>
2023-04-20 12:25:41 -0700

    Fix tests failures in actions

--- frontend/App.test.js
+++ frontend/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, waitFor, fireEvent } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
+import { act } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "./App";
 
